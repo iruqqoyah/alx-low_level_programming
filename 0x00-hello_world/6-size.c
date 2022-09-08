fc6f419bd4476303b11239b90179609c04973a2c
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-08 19:14:55 +0100

    sizeof operator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,12 @@ int main(void)
 {
 	char g;
 	int a;
-	double b;
 	long int c;
 	long long int d;
 	float f;
 
 	printf("Size of a char: %zu byte(s)", sizeof(g));
 	printf("\nSize of an int: %zu byte(s)", sizeof(a));
-	printf("\nSize of a double: %zu byte(s)", sizeof(b));
 	printf("\nSize of a long int: %zu byte(s)", sizeof(c));
 	printf("\nSize of a long long int: %zu byte(s)", sizeof(d));
 	printf("\nSize of a float: %zu byte(s)", sizeof(f));
