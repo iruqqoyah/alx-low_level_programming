5914508bb9405648e2cd8895a4bc85c3764e499d
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-08 17:25:24 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
+ *   
  *
- *    * Return: always 0
+ *     Return: always 0
  *
  * 
-    */
+ */
 
 int main(void)
 {
