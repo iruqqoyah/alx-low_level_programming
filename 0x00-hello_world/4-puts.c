28f5fc98239b43fe6a1f48128b28e1b943eff422
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-08 17:21:12 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,6 @@
 int main(void)
 {
 	/*this is for test*/
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\n"Programming is like building a multilingual puzzle");
 	return (0);
 }
