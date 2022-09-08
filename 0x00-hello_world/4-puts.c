e840adc9267be326d177efeda127d6dcfb83c973
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-08 17:15:24 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*this is for test*/
 int main(void)
 {
+	/*this is for test*/
 	char var1[] = "Programming is like building a multilingual puzzle";
 
-	puts(var1);
+	puts("%c\n", var1);
 	return (0);
 }
