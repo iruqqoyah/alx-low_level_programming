ede34abbc3d222d20b0afaa6b820cea3a814b872
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-11 16:33:52 -0700

    alphaBETH

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,8 +15,8 @@ int main(void)
 	char upper;
 	for (upper = 'a' ; upper <= 'z' ; upper++)
 		putchar(upper);
-		for (upper = 'A' ; upper <= 'Z' ; upper++)
-			putchar(upper);
-			putchar('\n');
+	for (upper = 'A' ; upper <= 'Z' ; upper++)
+		putchar(upper);
+	putchar('\n');
 			return (0);
 }
