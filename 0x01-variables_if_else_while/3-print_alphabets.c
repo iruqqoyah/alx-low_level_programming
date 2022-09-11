53720d2dee29a29927f5caa5eac2f01034c7bfff
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-11 16:41:25 -0700

    alphaBETH

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,16 @@
  * main - entry point
  *
  *
- * Return 0: success code is 0
+ * Return: success code is 0
  */
 
 
 int main(void)
 {
 	char upper;
-	for (upper = 'a' ; upper <= 'z' ; upper++)
+	for (upper = 'a'; upper <= 'z'; upper++)
 		putchar(upper);
-	for (upper = 'A' ; upper <= 'Z' ; upper++)
+	for (upper = 'A'; upper <= 'Z'; upper++)
 		putchar(upper);
 	putchar('\n');
 			return (0);
