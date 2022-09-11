5fadec0bd03bb7752063e0d0aca4539cfb7656f9
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-11 15:34:24 -0700

    alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * Return: return data
+ */
+
+int main()
+{
+	char alphabeth;
+
+	for (alphabeth = 'a' ; alphabeth <= 'z' ; alphabeth++)
+	{
+		puthchar(alphabeth);
+	}
+
+	return (0);
+}
