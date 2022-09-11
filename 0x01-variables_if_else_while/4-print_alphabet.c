353070f58b9d8f61931bc942143975d39f62e58c
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-11 16:50:36 -0700

    alphabeth without qe

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: main code
+ *
+ * Return: 1 if errors, otherwise 0
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch != 'q' && ch != 'e'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
