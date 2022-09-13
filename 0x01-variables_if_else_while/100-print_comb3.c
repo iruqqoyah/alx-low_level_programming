5100d3f81b1322e0e2ec615553daaa661fafb8e7
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-13 05:57:40 +0100

    comb5

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - entry point
  *
  *   *
@@ -10,54 +9,30 @@
  *
  *     *
  *
- *      */
+ */
 
 int main(void)
-
 {
-
 	int i;
-
 	int j;
-
-
-
 	for (i = 10; i <= 19; i++)
-
 	{
-
 		for (j = 10; j <= 19; j++)
-
 		{
-
 			if ((j % 10) > (i % 10))
-
 			{
-
 				putchar((i % 10) + '0');
 
-				putchar((j % 10) + '0');
-
 				if (i != 18 || j != 19)
-
 				{
-
 					putchar(',');
-
 					putchar(' ');
-
 				}
-
 			}
 
-
-
 		}
-
 	}
 
 	putchar('\n');
-
 	return (0);
-
 }
