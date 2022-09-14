f20e2ec217bfc0c16d41fa93b8d3b028891a21a9
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-14 05:42:06 +0100

    alphabeth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ **/
+
+int main(void)
+
+{
+
+    print_alphabet();
+
+    return (0);
+
+}
+
+void print_alphabeth(void)
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++);
+	putchar(ch)
+	putchar('\n')
+}
