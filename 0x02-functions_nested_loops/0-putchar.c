0a30a72f3a6359458d14520413be63b3ed86d51f
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-14 23:37:55 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,6 +18,6 @@ int main(void)
 		putchar(*ch);
 		ch++;
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 
 
-int putchar(char c);
+int _putchar(char c);
 
 void print_alphabet(void);
 
