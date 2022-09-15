157e390f6dc5457f9ec7fc75025f69a31b041a6c
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-15 04:38:12 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase letter
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c);
+{
+	int c;
+	if (c = 'A' && c <= 'Z')
+		return (0);
+	else if (c = 'a' && c <= 'z')
+		return (1);
+}
