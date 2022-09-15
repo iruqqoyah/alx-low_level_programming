d8d01079474306b4b7bc7b637386097a3f4a81d7
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-15 04:24:21 +0100

    10x alphabeth

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * Print 10x alphabet
  *
  * Return: 0
@@ -9,11 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-	int count=0;
-	while (count <10 )
+	int count = 0;
+
+	while (count < 10)
+
 	{
 		int ch;
-		for (ch='a'; ch<='z'; ch++)
+
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
