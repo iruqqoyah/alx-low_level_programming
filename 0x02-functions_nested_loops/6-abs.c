051731dd26f12831860caa700a26fa803221f49e
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-15 05:41:38 +0100

    last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - find absolute value of a number
- * @n - input number
+ * @n: input number
  * Return: 0, otherwise 1
  */
 
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: number
+ * Return: 0 
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
