2fb80a0ab9d94a44856129019ec961cec7263ab6
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-15 05:18:05 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * abs - find absolute value of a number
+ * Return: 0, otherwise 1
+ */
+
+int _abs(int);
+{
+
+	int n;
+	if (n < 0)
+		n = n * -1;
+		_putchar("%d\n", n);
+		return 0;
+}
