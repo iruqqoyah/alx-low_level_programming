f498de3624b37fe257c279006c042417c88ddeb8
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-21 05:26:00 +0100

    98 battery st

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -13,5 +13,5 @@ void swap_int(int *a, int *b)
 	int temp;
 	temp = *a;
 	*a = *b;
-	*b = temp;i
+	*b = temp;
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * int_strlen - lenght of string
+ * @s - string
+ * Return 0: of no errors
+ */
+
+int _strlen(char *s);
