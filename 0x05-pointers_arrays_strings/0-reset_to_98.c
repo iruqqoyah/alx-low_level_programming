938f5bb9c6f96aa9f7294b4d01400a452384af12
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-21 05:12:19 +0100

    98 battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,16 @@
+#include main.h
+
+/**
+ * main - 98 reset
+ * @n: n variable
+ * Description: testing mic
+ *
+ * Return: 0 if no errors
+ */
+
 void reset_to_98(int *n)
+
 {
 	int p = *n;
-	
-	_putchar("%x", p);
+	p = 98;
+}	
