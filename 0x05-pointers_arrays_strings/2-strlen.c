aac56bc4fcd545b6e4a0b0d443a964036fbed150
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-21 05:31:58 +0100

    lenstr

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * int_strlen - lenght of string
- * @s - string
+ * _strlen - lenght of string
+ * @s: string
+ * @count: count integer
  * Return 0: of no errors
  */
 
 int _strlen(char *s)
 {
 	int count = 0;
+
 	while (*(s + count) != '\0')
 		count++;
 	return (count);
