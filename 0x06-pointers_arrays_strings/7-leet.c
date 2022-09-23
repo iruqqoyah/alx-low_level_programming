dbc4eb56671d4fa64fc9ad36a5bb7f5f614f7e36
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-23 05:54:53 +0100

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *
+ *  * leet - encodes a string int: input string.
+ *
+ *   * Return: the pointer to dest.
+ *
+ *    */
+
+
+
+char *leet(char *Int count = 0, i;
+
+	int low_letters[] = {97, 101, 111, 116, 108};
+
+	int upp_letters[] = {65, 69, 79, 84, 76};
+
+	int numbers[] = {52, 51, 48, 55, 49};
+
+
+
+	while (*(s + count) != '\0')
+
+	{
+
+		for ( 5; {
+
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+
+			{
+
+				*(s + count) = numbers[i];
+
+				break;
+
+			}
+
+		}
+
+		count++;
+
+	}
+
+
+
+	return (s);
+
+}
