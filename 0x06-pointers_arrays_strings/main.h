45b2935c9d05d100e5edffd5595956695392f673
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-23 05:57:23 +0100

    rot

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *rot13(char *);
+char *leet(char *);
 
 #endif
