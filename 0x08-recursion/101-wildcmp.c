4e295697dbabb98c40bc44bba9b038bd30687b6f
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-29 00:48:38 +0100

    strcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * wildcmp - entry
+ * @s1: ist char
+ * @s2: 2nd char
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (wildcmp(s1, s2))
+		return 1;
+	else
+		return 0;
+}
