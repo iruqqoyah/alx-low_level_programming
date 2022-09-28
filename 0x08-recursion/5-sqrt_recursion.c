910b4e1f8643c6ddfcc40f1fd97e4a8e8adfada5
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 23:46:16 +0100

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 /**
- *
  * _sqrt_recursion - entry point
  * @n: integer num
  * Return: 0 always
@@ -11,10 +10,34 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
-		return (_sqrt_recursion(n));
+		return (squareroot(nc, (n + 1) / 2));
 	}
 	else
 		return (-1);
+}
+
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+
+int squareroot(int n, int i)
+{
+
+	if (i < 1)
+
+		return (-1);
+
+	else if (i * i == n)
+
+		return (i);
+
+	else
+
+		return (squareroot(n, i - 1));
+
 }
