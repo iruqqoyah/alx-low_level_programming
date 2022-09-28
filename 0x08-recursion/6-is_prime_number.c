15a4f418ff69ddf835d739d7c87bc91c4a2f95be
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-29 00:09:43 +0100

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number - entry point
+ * @n: num
+ * Return: 0 always
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 1 = 0 && n % n = 0)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
