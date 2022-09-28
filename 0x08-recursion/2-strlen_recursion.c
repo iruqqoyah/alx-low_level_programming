6054b5d60ebe0e5513a44b74d29f4975950a3029
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 18:13:12 +0100

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,8 +13,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		printf("%ld", strlen(s));
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (0);
+	else
+		return (0);
 }
