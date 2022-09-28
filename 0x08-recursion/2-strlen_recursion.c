af79d7937b01e98eb0976fb12ed3617f95594f86
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 17:57:19 +0100

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Entry point
+ *
+ * @s: input string
+ *
+ * Return: 0 always
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		printf("%d", strlen(s);
+	}
+	return 0;
+}
