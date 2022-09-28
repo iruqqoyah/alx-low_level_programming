94b0242a8d4a07ed62583ff5100c614dffb7dd13
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 18:46:57 +0100

    powerof

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _pow_recursion - entry point
+ *
+ * @x: base digit
+ *
+ * @y: power digit
+ *
+ * Return: 0 always
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return pow_recursion(x,y);
+	}
+}
