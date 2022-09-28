fa93b8ffb8fce7b97bee7319a0feb7d60b170a91
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 17:26:03 +0100

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,9 @@
  */
 
 
-void _puts_recursion(char *s){
-	
-	if(*s == '\0')
+void _puts_recursion(char *s) {
+	if (*s == '\0')
 		return;
 	printf("%c", *s);
-	_puts_recursion(s+1);
-	
-
+	_puts_recursion(s + 1);
 }
