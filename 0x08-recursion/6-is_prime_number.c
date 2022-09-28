43a44ca91bd65f411a50449086feeb5bc34368b9
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-29 00:13:23 +0100

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,10 +8,16 @@
 
 int is_prime_number(int n)
 {
-	if (n % 1 = 0) && (n % n = 0)
+	if (n % 1 = 0)
+	{
+		return (1);
+	}
+	else if (n % n = 0)
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
 }
