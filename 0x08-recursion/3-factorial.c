903e111836f40ba06bb00bc1550ba4911434ee28
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 18:19:03 +0100

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * factorial - entry point
+ * @n: input number
+ * Return: 0 always
+ */
+
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	else
+		return (factorial s * (s-1));
+}
