66e18bc97119f25c0046297c767a97299f490c2e
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-28 05:46:16 +0100

    strbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(chchar *src, unsigned int n);
 char *_strchr(chr c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 
 
