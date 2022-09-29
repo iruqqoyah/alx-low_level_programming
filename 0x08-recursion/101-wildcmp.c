e1f59845ec8b3b6d24e9d0eafb7e5ef165a2cad8
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-09-29 01:15:29 +0100

    strcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 
 		return (0);
 
-	if (*s1 == '\0' && *s2 == '\0'
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
 	if (*s1 == *s2)
@@ -27,4 +27,3 @@ int wildcmp(char *s1, char *s2)
 
 	return (0);
 }
-
