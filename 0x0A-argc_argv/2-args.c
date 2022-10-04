d743617c632c067b2ccede849f2c13e86ecc8515
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-10-04 16:00:02 +0100

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count element
+ * @argv: array of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc1);
+	return (0);
+}
