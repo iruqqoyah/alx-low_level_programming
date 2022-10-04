7bd3feeb8183b9b1e5b2916c2b5d9151dd8a3129
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-10-04 15:49:27 +0100

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(argc, char* argv[])
+/**
+ * main - entry point
+ * @argc: count element
+ * @argv: array of argc
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char* argv[])
 {
 
-	printf("the name of this file is %d", argc);	
+	printf("the name of this file is %s\n", argv[0]);	
 
 return (0);
 }
