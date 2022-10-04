9f308edc3fde7e9dabad76180fef366467c35a6b
Ishola Rukayat Adejoke <rukaishola@gmail.com>
2022-10-04 16:10:44 +0100

    argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - entry point
@@ -13,7 +12,7 @@ int main(int argc, char *argv[])
 
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 
