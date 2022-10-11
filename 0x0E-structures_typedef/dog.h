feb0a88d3cbe4b91eee4aab619ccac6c11613309
Ishola Rukayat <rukaishola@gmail.com>
2022-10-11 11:43:06 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - entry point
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: nothing
+ */
+
 struct dog
 {
 	char* name;
