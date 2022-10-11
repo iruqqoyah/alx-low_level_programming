ec7e4a4c3336b773dfffbe05e305b8db83627546
Ishola Rukayat <rukaishola@gmail.com>
2022-10-11 12:07:21 +0100

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include dog.h
+
+/**
+ * init_dog - function name
+ * @d: typedef name
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void int_dog(struct dog *d, char *name, float age, char *ownner)
+{
+	if(d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
