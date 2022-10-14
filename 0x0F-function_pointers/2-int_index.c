4d4d4a130f5d55438cbd02877a1904e79b35867d
Ishola Rukayat <rukaishola@gmail.com>
2022-10-14 13:05:26 +0100

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function name
+ * @array: array name
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: 0 if no errors
+ */
+
+int int_index(int * array, int size, int (*cmp)(int))
+{
+	if(array && cmp){
+	       if (size <= 0)
+	       	return (-1);
+	
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
