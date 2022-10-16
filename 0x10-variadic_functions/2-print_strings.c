eaceadfaeb2791874dc85a6dbdd7c17c8e84ff15
Ishola Rukayat <rukaishola@gmail.com>
2022-10-16 15:31:44 +0100

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, int);
+		printf("%s", va_arg(ap, char *);
 		if (separator == NULL && i = (n - 1))
 			printf("nil");
 	}
