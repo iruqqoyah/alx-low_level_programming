1a0365c6f016ec6277899fd9589daaa2561a190f
Ishola Rukayat <rukaishola@gmail.com>
2022-10-16 22:27:00 +0100

    dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,31 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: struct dog
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
 	int i, lname, lowner;
 
 	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog ==NULL || !(name) || !(name) || !(owner))
+	if (p_dog == NULL || !(name) || !(owner))
 	{
 		free(p_dog);
 		return (NULL);
 	}
 
-	for (lname = 0l name[lname]; lname++)
+	for (lname = 0; name[lname]; lname++)
 		;
 
-	for(lowner = 0; owner[lowner]; lowner++)
+	for (lowner = 0; owner[lowner]; lowner++)
 		;
 
 	p_dog->name = malloc(lname + 1);
@@ -34,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog->owner);
 		free(p_dog->name);
 		free(p_dog);
-		return (NULL)
+		return (NULL);
 	}
 
 	for (i = 0; i < lname; i++)
@@ -42,7 +44,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p_dog->name[i] = '\0';
 
 	p_dog->age = age;
-
 
 	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
