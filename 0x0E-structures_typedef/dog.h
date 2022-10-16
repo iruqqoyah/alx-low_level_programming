fa2116e1ad526b21e0902c718385a63e7d6a8970
Ishola Rukayat <rukaishola@gmail.com>
2022-10-16 18:29:20 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-Void print-dog(struct dog *d);
+void print-dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d)
 
