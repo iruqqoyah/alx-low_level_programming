b95fd9c2bde0412b2f1dd2d908d498157d34a4f9
root <root@server.example.com>
2022-10-29 00:48:19 +0100

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int i = 0, int base = 1;
+	int i;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (b[i + 1])
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		i++;
-	}
-
-	while (i >= 0)
-	{
-		result += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
+	
+		result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 
